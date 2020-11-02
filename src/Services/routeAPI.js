411ff8c25bbf2fcac0a72c2f608fe9d5798818a2
ngodongdacc <ngodongdac3005@gmail.com>
2020-11-02 14:34:39 +0700

    api

--- src/Services/routeAPI.js
+++ src/Services/routeAPI.js
@@ -1,4 +1,5 @@
-export const linkAPI = "https://localhost:4000/"
+export const linkAPI = "https://103.130.214.13:3005/"
+//"https://localhost:4000/"
 // http://103.130.214.106:3005/
 // http://118.69.32.128:5005/
 
